dc6ce91a6edb24ee8a91e8aec672b3aa6777f75f
JD Singh <jagdeepsingh@founderandlightning.com>
2023-01-16 16:52:49 +0530

    Add multi-colored circles for multi-theme testing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex items-center justify-between">
+            <h2 class="font-semibold text-xl leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+            <div class="flex items-center space-x-4 h-8">
+                <div class="h-full w-8 rounded-full border-4 border-gray-500"></div>
+                <div class="h-full w-8 rounded-full border border-gray-500 bg-red-500"></div>
+                <div class="h-full w-8 rounded-full border border-gray-500 bg-blue-500 "></div>
+                <div class="h-full w-8 rounded-full border border-gray-500 bg-green-500"></div>
+                <div class="h-full w-8 rounded-full border border-gray-500 bg-gray-50"></div>
+            </div>
+        </div>
     </x-slot>
+
 
     <div class="p-2">
         <div class="p-6 max-w-7xl border-4 border border-dashed dark:border-gray-700">
